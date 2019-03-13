582bb7323f8b95b416efe56b3dfa814432a8d537
skywalker512 <houzhenhong@hotmail.com>
2019-03-13 11:13:49 +0800

    [提交] 暂时不使用 babel 来启动

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,7 @@ const { paths } = require('react-app-rewired');
 const path = require('path');
 
 // 为了不 eject 项目所以使用 react-app-rewired 来重置 webpack 配置
+// 暂时没有必要用 babel来启动 所以就 require 来引入
 
 module.exports = function override(config, env) {
   const alias = {
