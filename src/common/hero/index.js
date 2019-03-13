84c1d8efa356a21888e5c9a514094743a70c70dd
skywalker512 <houzhenhong@hotmail.com>
2019-03-13 11:37:18 +0800

    [提交] hero 主体

---
+++ src/common/hero/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+import {
+  HeroWrapper,
+} from './style'
+
+export default class Hero extends Component {
+  render() {
+    return (
+      <HeroWrapper>
+        
+      </HeroWrapper>
+    )
+  }
+}
---
+++ src/common/hero/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const HeroWrapper = styled.section`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  color: #c7ccd8;
+  background-color: #1e2430;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
