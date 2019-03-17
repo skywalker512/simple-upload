e1ffd02e4be13db1c31bbd4c8b4e52c2b16c653d
skywalker512 <houzhenhong@hotmail.com>
2019-03-17 11:55:34 +0800

    [提交] saga 的一些注释

--- src/common/upload/store/saga.js
+++ src/common/upload/store/saga.js
@@ -1,22 +1,30 @@
-import { takeEvery, all, put } from 'redux-saga/effects'
+import { takeEvery, all, put, fork } from 'redux-saga/effects'
 import ajax from '@/utils/ajax'
 import * as constants from './constants'
 
 async function putFile (action) {
   try {
-    const a = await ajax('POST', '/upload', action.value.toJS())
-    put(constants.FILE_UPLOAD)
+    // 这里可以使用 saga 的 api: call(ajax, 'POST', '/upload', action.value.toJS())
+    // call 相当于同步命令
+    // 但是 await 写起来方便一些
+    const a = await ajax('POST', '/upload', action.res.toJS())
+    console.log(a)
+    // put(constants.FILE_UPLOAD)
   } catch (error) {
     console.log(error)
   }
 }
 
 function* watchFileUpload () {
+  // takeEvery 是一个使用 take 和 fork 构建的高级 API
+  // TODO -------- 使用 take race 取消上传
   yield takeEvery(constants.FILE_UPLOAD, putFile);
 }
 
 function* sagas() {
-  yield all([watchFileUpload()])
+  // all 相当于是 Promise#all 会等待全部执行完毕
+  // fork 相当于异步命令 call 相当于同步命令
+  yield all([fork(watchFileUpload)])
 }
 
 export default sagas;
--- src/store/saga.js
+++ src/store/saga.js
@@ -1,11 +1,11 @@
-import { all } from 'redux-saga/effects'
+import { all, fork } from 'redux-saga/effects'
 
 import { saga as upload} from '@/common/upload/store'
 
 // saga 做了检查必须是这种类型
 // generator 是一种全新的方法, 在调用的时候与 async 完全不同
 function* mySaga() {
-  yield all([upload()])
+  yield all([fork(upload)])
 }
 
 export default mySaga;
