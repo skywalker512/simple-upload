2e0527fbdf306f7753daa73ccaae68fb07fb7201
skywalker512 <houzhenhong@hotmail.com>
2019-03-17 12:36:15 +0800

    [提交] 使用 深度操作的 api

--- src/common/upload/store/reducer.js
+++ src/common/upload/store/reducer.js
@@ -14,11 +14,11 @@ const defaultState = fromJS({
 export default (state = defaultState, action) => {
   switch (action.type) {
     case constants.FILE_CHANGE:
-      return state.set('file', state.get('file').push(action.value))
+      return state.setIn(['file', -1], action.value)
     case constants.FILE_REMOVE:
-      return state.set('file', state.get('file').delete(action.index))
+      return state.deleteIn(['file', action.index])
     // case constants.REQUEST_POST:
-    //   return state
+    //   return state.set('file', state.get(''))
     // case constants.RECEIVE_POST:
     //   return state
     default:
