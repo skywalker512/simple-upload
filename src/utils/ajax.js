7747acc789c57d08c1640b389b339f5df84e7084
skywalker512 <houzhenhong@hotmail.com>
2019-03-17 10:15:10 +0800

    [提交] ajax 问题

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -14,16 +14,13 @@ export default (type='GET', url, data) => new Promise((reslove, reject) => {
   xhr.send(sendData)
 
   xhr.onreadystatechange = () => {
-    // xhr.readyState === 4 也可以用 XMLHttpRequest.DONE 代替。XMLHttpRequest.DONE 的计算结果就是 4。
-    if (XMLHttpRequest.DONE) {
+    if (xhr.readyState === 4) {
       //根据服务器的响应内容格式处理响应结果
       if (xhr.status === 200) {
         reslove(xhr.responseText)
       } else {
         reject('请求未成功')
       }
-    } else (
-      reject('未发出请求')
-    )
+    }
   }
 });
